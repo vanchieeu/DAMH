b181e0e150eb829f8185673e563aa9e3eaae5cea
_vanchieeu <95594868+vanchieeu@users.noreply.github.com>
2024-05-07 00:35:40 +0700

    update gui

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtWidgets import QApplication,QMainWindow, QFileDialog
 from GUI import Ui_MainWindow
+from PyQt5.QtGui import QPixmap
 
 class MainWindow:
     def __init__(self):
         self.main_win = QMainWindow()
         self.uic = Ui_MainWindow()
         self.uic.setupUi(self.main_win)
+        self.uic.browser.clicked.connect(self.linkto)
 
+
+    def linkto(self):
+        link = QFileDialog.getOpenFileName(filter='*.jpg *png')
+        self.uic.screen.setPixmap(QPixmap(link[0]))
+        self.uic.line_Edit.setText(link[0])
 
 
     def show(self):
