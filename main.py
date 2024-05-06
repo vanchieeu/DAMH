3e3e75018818e8a6791cfdf3b0d486f9a6e0341a
_vanchieeu <95594868+vanchieeu@users.noreply.github.com>
2024-05-06 23:24:40 +0700

    GUI done

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from GUI import Ui_MainWindow
+
+class MainWindow:
+    def __init__(self):
+        self.main_win = QMainWindow()
+        self.uic = Ui_MainWindow()
+        self.uic.setupUi(self.main_win)
+
+
+
+    def show(self):
+        self.main_win.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec())
