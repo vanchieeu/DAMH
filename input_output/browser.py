71a2f9cd1d9e3149458f62d9dfacbc871ae6c7d6
_vanchieeu <95594868+vanchieeu@users.noreply.github.com>
2024-06-18 11:40:58 +0700

    update

--- input_output/browser.py
+++ input_output/browser.py
@@ -1,11 +1,17 @@
 from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtGui import QPixmap
 import os
+import cv2
 
 def linkto(main_window):
     link = QFileDialog.getOpenFileName(filter='*.jpg *.png')
     if link[0]:
         main_window.image_path = link[0]
+
+        img = cv2.imread(link[0])
+        height, width, channels = img.shape
+        main_window.uic.screen.setGeometry((671-10)+1, 90, height, width)
+
         main_window.uic.screen.setPixmap(QPixmap(main_window.image_path))
         main_window.uic.line_Edit.setText(main_window.image_path)
         main_window.uic.label_2.show()
